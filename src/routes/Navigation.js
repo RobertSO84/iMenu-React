5804cf408a15c1594a4ea65734ebc8369a48eb41
robertso <robertso@RobertPC.localdomain>
2022-02-17 23:59:13 -0600

    Change React Router V6

---
+++ src/routes/Navigation.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Routes } from "react-router-dom"
+
+export function Navigation() {
+    return (
+        <Routes>
+            
+            <h2>Navigation...</h2>
+
+            
+        </Routes>
+    )
+}
