af0b9b14cd120ed236b26f724431a382fc63538d
robertso <robertso@RobertPC.localdomain>
2022-03-03 11:20:50 -0600

    Add pending orders to list tables

--- src/api/orders.js
+++ src/api/orders.js
@@ -2,7 +2,11 @@ import { BASE_API } from "../utils/constants";
 
 export async function getOrdersByTableApi(idTable, status = "", ordering = "") {
     try {
-        const url = `${BASE_API}/api/orders/?`;
+        const tableFilter = `table=${idTable}`;
+        const statusFilter = `status=${status}`;
+        const closeFilter = 'close=False';
+
+        const url = `${BASE_API}/api/orders/?${tableFilter}&${statusFilter}&${closeFilter}&${ordering}`;
         const response = await fetch(url);
         const result = await response.json();
         return result;
--- src/components/Admin/Table/TableAdmin/TableAdmin.js
+++ src/components/Admin/Table/TableAdmin/TableAdmin.js
@@ -1,13 +1,29 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
+import { Label } from "semantic-ui-react";
+import { size, sortedIndex } from "lodash";
+import { getOrdersByTableApi } from "../../../../api/orders";
+import { ORDER_STATUS } from "../../../../utils/constants";
 import { ReactComponent as IcTable } from "../../../../assets/mesa7.svg";
 import "./TableAdmin.scss";
 
 export function TableAdmin(props) {
     const { table } = props;
+    const [orders, setOrders] = useState([]);
+
+
+    useEffect(() => {
+        (async () => {
+            
+            const response = await getOrdersByTableApi(table.id, ORDER_STATUS.PENDING
+            );
+            setOrders(response);
+        })();
+    }, []);
 
     return (
         <div className="table-admin">
+            { size(orders) > 0 ? <Label circular color="orange">{size(orders)}</Label> : null}
+
             <IcTable />
             <p>Mesa {table.number}</p>
             
