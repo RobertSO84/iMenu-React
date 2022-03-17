7848e7086220bd938195dd594c42641396a0292f
robertso <robertso@RobertPC.localdomain>
2022-03-17 02:38:54 -0600

    Add category client page

--- src/layouts/ClientLayout/ClientLayout.js
+++ src/layouts/ClientLayout/ClientLayout.js
@@ -1,13 +1,56 @@
-import React from 'react'
-import "./ClientLayout.scss"
+import React, { useEffect } from "react";
+import { Container, Icon, Button } from "semantic-ui-react";
+import { useParams, useNavigate, Link } from "react-router-dom";
+import { useTable } from "../../hooks";
+import "./ClientLayout.scss";
 
-export function ClientLayout( props ) {
-    const { children } = props;
-    return (
-        <div>
-            <p>Client Layout</p>
+export function ClientLayout(props) {
+  const { children } = props;
+  const { isExistTable } = useTable();
+  const { tableNumber } = useParams();
+  const navigate = useNavigate();
 
-            { children }
+  useEffect(() => {
+    (async () => {
+      const exist = await isExistTable(tableNumber);
+      if (!exist) closeTable();
+    })();
+  }, [tableNumber]);
+
+  const closeTable = () => {
+    navigate("/");
+  };
+
+  const goTopCart = () => {
+    navigate(`/client/${tableNumber}/cart`);
+  };
+
+  const goToOrders = () => {
+    navigate(`/client/${tableNumber}/orders`);
+  };
+
+  return (
+    <div className="client-layout-bg">
+      <Container className="client-layout">
+        <div className="client-layout__header">
+          <Link to={`/client/${tableNumber}`}>
+            <h1>iMenu</h1>
+          </Link>
+          <span>Mesa {tableNumber}</span>
+          <div>
+            <Button icon onClick={goTopCart}>
+              <Icon name="shop" />
+            </Button>
+            <Button icon onClick={goToOrders}>
+              <Icon name="list" />
+            </Button>
+            <Button icon onClick={closeTable}>
+              <Icon name="sign-out" />
+            </Button>
+          </div>
         </div>
-    )
+        <div className="client-layout__content">{children}</div>
+      </Container>
+    </div>
+  );
 }
