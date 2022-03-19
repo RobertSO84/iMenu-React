5498149f147f048b74a0b4edafd5fbaee958445f
robertso <robertso@RobertPC.localdomain>
2022-03-18 20:04:06 -0600

    to deploy in netlify

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,8 @@
 // Pasar a variables de entorno
 
-export const BASE_API = "http://127.0.0.1:8000";
+export const BASE_API = "https://imenu-django.herokuapp.com";
+//http://127.0.0.1:8000
+
 export const TOKEN = "token";
 
 export const ORDER_STATUS = {
